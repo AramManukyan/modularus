eec2190ea55a282b99a37a98b5bc6456aae63471
Aram Manukyan <aram.manukyan92@gmail.com>
2014-10-11 13:39:38 +0400

    Widgets directives some fixes

--- src/reports/widgets/linechart/linechart-controller.js
+++ src/reports/widgets/linechart/linechart-controller.js
@@ -5,25 +5,44 @@ exampleApp.widgets
 	var data = $parse($scope.linechhartData)($scope.linechhartData);
 	nv.addGraph(function() {
 	var chart = nv.models.pieChart()
-	  .x(function(d) { return d.label })
-	  .y(function(d) { return d.value })
-	  .showLabels(true)     //Display pie labels
-	  .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
-	  .labelType("percent") //Configure what type of data to show in the label. Can be "key", "value" or "percent"
-	  .donut(true)          //Turn on Donut mode. Makes pie chart look tasty!
-	  .donutRatio(0.35)     //Configure how big you want the donut hole size to be.
-	  ;
+		  .x(function(d) { return d.label })
+		  .y(function(d) { return d.value })
+		  .showLabels(true)     //Display pie labels
+		  .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
+		  .labelType("percent") //Configure what type of data to show in the label. Can be "key", "value" or "percent"
+		  .donut(true)          //Turn on Donut mode. Makes pie chart look tasty!
+		  .donutRatio(0.35)     //Configure how big you want the donut hole size to be.
+		  ;
 
-	d3.select("#linechart")
-		.append('svg')
-	    .datum(data)
-	    .transition().duration(350)
-	    .call(chart);
+		d3.select("#linechart1")
+			.append('svg')
+		    .datum(data)
+		    .transition().duration(350)
+		    .call(chart);
 
-	nv.utils.windowResize(function(){
-		chart.update();
+		nv.utils.windowResize(function(){
+			chart.update();
+		});
+
+		return chart;
 	});
-	
-	return chart;
+
+	nv.addGraph(function() {
+		var chart = nv.models.pieChart()
+		  .x(function(d) { return d.label })
+		  .y(function(d) { return d.value })
+		  .showLabels(true);
+
+		d3.select("#linechart2")
+			.append('svg')
+		    .datum(data)
+		    .transition().duration(350)
+		    .call(chart);
+
+		nv.utils.windowResize(function(){
+			chart.update();
+		});
+
+		return chart;
 	});
 });
