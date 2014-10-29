38606a59a8db6a9cf65d3b7318bbec70b151ecca
Gevorg Harutyunyan <gevharut@gmail.com>
2014-10-29 21:43:14 +0400

    Dropdowns directive fix

--- src/_common/dropdowns/dropdowns-directive.js
+++ src/_common/dropdowns/dropdowns-directive.js
@@ -5,41 +5,23 @@ sbAdmin.common
 
   function link(scope, $element) {
 
-    var $dropdowns = null,
-        $dropdownLists = null;
-
     $(document).on("click", function(e) {
 
       var $dropdownToggle = $(e.target).closest(".dropdown-toggle");
 
-      if(!$dropdownToggle.length) {
-        return false;
+      if($dropdownToggle.length) {
+        var $dropdowns = $element.find(".dropdown");
+        var $dropdownLists = $dropdowns.find("> ul");
+        $dropdowns.not($dropdownToggle.parent()).filter(".open").removeClass("open");
+        $dropdownToggle.parent().toggleClass("open");
       }
 
-      $dropdowns = $element.find(".dropdown");
-      $dropdownLists = $dropdowns.find("> ul");
-
-      $dropdowns.not($dropdownToggle.parent()).filter(".open").removeClass("open");
-      $dropdownToggle.parent().toggleClass("open");
-
     });
-    
-    // $dropdowns.find(".dropdown-toggle").click(function() {
-
-    //   $dropdowns.not($(this).parent()).filter(".open").removeClass("open");
-    //   $(this).parent().toggleClass("open");
-
-    //   return false;
-
-    // });
 
     $(document).on("click", function(event) {
 
-
-      if(!$(event.target).hasClass("dropdown-toggle") && $dropdowns) {
-
-        $dropdowns.filter(".open").removeClass("open");
-
+      if(!$(event.target).hasClass("dropdown-toggle")) { //&& $dropdowns
+        $(".dropdown").filter(".open").removeClass("open");
       }
 
     });
